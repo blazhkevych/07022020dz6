f825c77902286fcde9cc7d3c232cd27e53dfaf4a
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 19:38:38 +0300

    Добавьте файлы проекта.

---
+++ 07022020dz6.cpp
@@ -0,0 +1,26 @@
+﻿/*
+Задание 6: Пользователь вводит целое число.Необходимо вывести все целые числа, на которое заданное число делиться без остатка.
+*/
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	int num;
+	cout << "Введите целое число: \n";
+	cin >> num;
+	cout << "Все целые числа, на которые заданное число делиться без остатка:\n";
+	num = (num < 0) ? -num : num;
+	for (int i = -num; i <= num; i++)
+	{
+		if (i == 0) continue;
+		if (num % i == 0)
+		{
+			cout << i << ' ';
+		}
+	}
+	cout << endl;
+	return 0;
+}
